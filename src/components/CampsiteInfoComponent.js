38cb3fdbf965a97d84afdc4302bcdeae50626fc9
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-03-11 01:31:01 -0600

    Breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
   function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1">  
             <Card>
                 <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
+                    <CardBody> 
                         <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>
@@ -42,6 +41,16 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
              return  (
                  <div className="container">
                     <div className="row">
+                         <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                             <h2>{props.campsite.name}</h2>
+                             <hr />
+                          </div>
+                    </div>
+                    <div className="row">
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments comments={props.comments} />
                     </div>
@@ -52,6 +61,5 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
              return <div />  
         
     }     
-
 
 export default CampsiteInfo;
