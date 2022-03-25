43439e580ea99a65e27eb30142ed93d2a100a22a
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-03-25 01:18:10 -0500

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, Label, ModalBody, ModalHeader  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -128,6 +129,28 @@ class CommentForm extends Component {
     }
 
   function CampsiteInfo(props) {
+      if (props.isLoading) {
+          return (
+              <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+              </div>
+          );
+      }
+
+      if (props.errMess) {
+        return (
+            <div className="container">
+              <div className="row">
+                  <div className="col">
+                     <h4>{props.errMess}</h4>
+                  </div>
+              </div>
+            </div>
+        );
+      }
+
         if (props.campsite) {
              return  (
                  <div className="container">
